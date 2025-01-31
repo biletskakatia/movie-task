179d49f0a00d558bab12599468ea4ef99af89729
biletskakatia <152877295+biletskakatia@users.noreply.github.com>
2025-01-31 20:26:46 +0200

    8

--- frontend/src/Redux/moviesOps.js
+++ frontend/src/Redux/moviesOps.js
@@ -1,7 +1,7 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from "axios";
 
-axios.defaults.baseURL = 'http://localhost:3000';
+axios.defaults.baseURL = 'https://movie-task-1-g6q8.onrender.com';
 
 export const fetchMovies = createAsyncThunk(
     'movies/fetchAll',
